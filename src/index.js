266c76499feb1c7b3babb23dd3d474f595ad916f
Sean Macharia <Snmuanzi@gmail.com>
2026-04-19 18:36:24 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form")
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const input = document.querySelector("#new-task-description")
+    const task = input.value 
+
+    buildToDo(task)
+
+    form.reset()
+
+  })
 });
+
+function buildToDo(task) {
+  const taskList = document.querySelector("#tasks")
+
+  const li = document.createElement("li")
+  li.textContent = task
+  taskList.appendChild(li)
+}
